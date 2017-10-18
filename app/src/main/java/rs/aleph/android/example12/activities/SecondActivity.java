5259549e5919acc927809b04935e1c3dc617a85d
eufrat15 <futavcina@gmail.com>
2017-10-18 19:23:01 +0200

    sreda prvi commit

--- app/src/main/java/rs/aleph/android/example12/activities/SecondActivity.java
+++ app/src/main/java/rs/aleph/android/example12/activities/SecondActivity.java
@@ -35,7 +35,7 @@ public class SecondActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_second_relative);
 
         final int position = getIntent().getIntExtra("position", 0);
 
